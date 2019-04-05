32f5857a6127e8588c2cfecf4173db4e91ca4814
Yohei Osanai <osanay@gmail.com>
2019-04-05 22:15:15 +0900

    Use --disable-dev-shm-usage option
    cf. https://developers.google.com/web/tools/puppeteer/troubleshooting#tips

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Capybara.register_driver :selenium do |app|
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options).tap do |driver|
     driver.browser.manage.window.size = Selenium::WebDriver::Dimension.new(1280, 1024)
